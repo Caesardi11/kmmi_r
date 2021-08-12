d2b60b3daad9cc798191f0c217aad6a171125c3d
Caesardi11 <111202012701@mhs.dinus.ac.id>
2021-08-12 15:26:22 +0900

    third commit

--- hello.r
+++ hello.r
@@ -1,5 +1,19 @@
-teks1 = "Selamat datang di Dunia R"
-teks2 = "Anda akan terkesima!!!"
+# Data Frame
+id <- c(1,2,3,4) #vektor kolom id
+nama <- c("Mamat", "Sutatang", "Honda", "Suzuki") #vektor kolom nama
+IPK <- c(3.8,3.7,4.0,2.9) #vektor kolom IPK
 
-teks1
-teks2
+# membuat data frame dari kolom vektor
+mydata <- data.frame(id,nama,IPK)
+
+# menambahkan nama kolom
+names(mydata) <- c("ID","Nama","IPK")
+print(mydata)
+
+# mengambil 2 kolom
+mydata[2:3]
+
+# mengambil satu kolom dengan nama "IPK”
+mydata$IPK
+
+
